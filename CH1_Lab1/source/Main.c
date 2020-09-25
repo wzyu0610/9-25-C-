c236611ebe79f6974e6ddf5e12290dbd79d00cc1
wzyu0610 <0610wuwu@gmail.com>
2020-09-25 21:15:51 +0800

    first

---
+++ CH1_Lab1/source/Main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Welcome to C\n");
+	system("pause");
+	return 0;
+}
---
+++ CH1_Lab2/source/User_defined.c
@@ -0,0 +1,6 @@
+#include <stdio.h>;
+int User_defined_print()
+{
+	printf("Welcome to C\n");
+	return 0;
+}
